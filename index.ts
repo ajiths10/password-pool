7e95fff9fa0359fe1c81f2529727e946f28c8fa5
ajiths10 <ajithsnair10@gmail.com>
2023-04-11 20:48:01 +0530

    variable scope changed to modern es6

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 const generate = (l: number = 8): string => {
-  var pass = "";
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+  let pass = "";
+  const str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
   for (let i = 1; i <= l; i++) {
-    var char = Math.floor(Math.random() * str.length + 1);
+    let char = Math.floor(Math.random() * str.length + 1);
     pass += str.charAt(char);
   }
 
